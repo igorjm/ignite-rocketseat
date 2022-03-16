d3d35bb0bd37e49b0376230eb725f694e84d5fb9
IMelo <igor.melo@cognyte.com>
2022-03-16 19:53:32 -0300

    steps to add typescript

--- reactjs/01-github-explorer/src/components/RepositoryItem.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryItem.jsx
@@ -1,3 +1,14 @@
+/*
+interface RepositoryItemProps {
+    repository: {
+        name: string;
+        description: string;
+        html_url: string;
+    }
+}
+*/
+
+// export function RepositoryItem(props: RepositoryItemProps) {
 export function RepositoryItem(props) {
     return (
         <li>
